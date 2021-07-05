46f9a258066f5f3391221488997372bcc66f6a6d
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-07-05 14:38:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,7 @@ resource "vault_aws_secret_backend" "aws" {
   secret_key = "SECRETKEYFROMAWS"
 }
 
+resource "aws_iam_access_key" "lb" {
+  user    = aws_iam_user.lb.name
+  pgp_key = "keybase:some_person_that_exists"
+}
