ce0c08303235df7cc49230296507b93285840a61
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-07-05 12:47:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,19 @@ resource "kubernetes_secret" "example" {
 
   type = "kubernetes.io/basic-auth"
 }
+
+data "azurerm_key_vault" "terrakv" {
+  name                = "terrakv" // KeyVault name
+  resource_group_name = "mykv" // resourceGroup
+}
+
+data "azurerm_key_vault_secret" "kvsecret" {
+name = "secret" // Name of secret
+key_vault_id = data.azurerm_key_vault.terrakv.id
+}
+
+os_profile {
+computer_name  = "vm-01"
+admin_username = "testadmin"
+admin_password = data.azurerm_key_vault_secret.kvsecret.value // Toget actual value
+}
