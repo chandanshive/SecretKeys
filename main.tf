11430eab13d96e813c0ff880f429ea5210b156ff
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-07-05 14:31:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,16 @@ resource "vault_aws_secret_backend" "aws" {
   access_key = "AKIA....."
   secret_key = "SECRETKEYFROMAWS"
 }
+
+# azure code
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "abcd1234"
+    key                  = "abc9923xyz"
+
+    # rather than defining this inline, the Access Key can also be sourced
+    # from an Environment Variable - more information is available below.
+    access_key = "abcdefghijklmnopqrstuvwxyz0123456789..."
+  }
+}
